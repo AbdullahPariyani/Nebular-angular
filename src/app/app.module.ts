024ffc22124d302826c12616c79f2b8d87a2587c
vijeta.rathod <vijeta.rathod@zymr.com>
2022-04-06 15:08:16 +0530

    Auth basepath strategies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -55,10 +55,32 @@ import { HeaderComponent } from './shared/component/header/header.component';
       strategies: [
         NbPasswordAuthStrategy.setup({
           name: 'email',
+         
+          baseEndpoint: 'http://example.com/app-api/v1',
+          login: {
+            endpoint: '/auth/sign-in',
+            method: 'post',
+          },
+          register: {
+            endpoint: '/auth/sign-up',
+            method: 'post',
+          },
+          logout: {
+            endpoint: '/auth/sign-out',
+            method: 'post',
+          },
+          requestPass: {
+            endpoint: '/auth/request-pass',
+            method: 'post',
+          },
+          resetPass: {
+            endpoint: '/auth/reset-pass',
+            method: 'post',
+          },
         }),
       ],
       forms: {},
-    }),
+    }), 
   ],
   providers: [HttpService],
   bootstrap: [AppComponent]

9bc7023723cac4f389f752b4ae517b26e4ce85b0
abdullah-1529 <abdullah.pariyani@zymr.com>
2022-04-06 12:18:21 +0530

    demo form

--- src/app/shared/component/header/header.component.ts
+++ src/app/shared/component/header/header.component.ts
@@ -56,26 +56,30 @@ export class HeaderComponent implements OnInit {
     {
       title: 'Demo From',
       icon: 'bar-chart-outline',
-      link: 'demo-form'
+      link: '/demo-form'
+    }, {
+      title: 'Functional App',
+      icon: 'cloud-upload-outline',
+      link: '/functional-app'
     }, {
       title: 'Login',
       icon: 'person-outline',
-      link: 'auth/login'
+      link: '/auth/login'
     },
     {
       title: 'Register',
       icon: 'person-add-outline',
-      link: 'auth/register'
+      link: '/auth/register'
     },
     {
       title: 'Forgot',
       icon: { icon: 'checkmark-outline', pack: 'eva' },
-      link: 'auth/forgot'
+      link: '/auth/forgot'
     },
     {
       title: 'Logout',
       icon: 'unlock-outline',
-      link: 'auth/logout'
+      link: '/auth/logout'
     },
   ];
 
